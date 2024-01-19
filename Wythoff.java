4e134eef94f675b12112432ed8e2c3e6e0c8cf9b
Omer <113857370+OmerGS@users.noreply.github.com>
2024-01-19 14:48:38 +0100

    AI model
    This version allow to play in different mod like human vs human or 3 different level of AI.

--- Wythoff.java
+++ Wythoff.java
@@ -2,14 +2,22 @@
 * Ce programme peremet de jouer au jeu de Wythoff.
 * @author O.Gunes
 */
-class Wythoff1{
-	int positionX;
-	int positionY;
-	String joueurActuelle;
-	char plateau[][];
-	int compteurPartie = 1;
-	int compteurJoueur1 = 0;
-	int compteurJoueur2 = 0;
+class Wythoff2{
+	final double PHI = (1+Math.sqrt(5))/2; //Constante mathématique utilisé pour deduire les positions gagnantes
+	int difficulte; //Difficulté du jeu
+	
+	char plateau[][]; //Le plateau de jeux
+	int positionX; //Coordonée X du pion
+	int positionY; //Coordonée Y du pion
+	
+	String joueurActuelle; //Nom du joueur qui joue actuellement
+	String joueur1; //Joueur humain
+	String ordinateur; //La pseudo-IA
+	
+	int compteurIA = 0; //Compteur de point de l'IA
+	int compteurJoueur = 0; //Compteur de point Joueur
+	int compteurPartie = 1; //Compteur de partie
+	
 	
 	void principal(){
 		jouer(); //Cette methode permet de jouer au Wythoff
@@ -17,7 +25,7 @@ class Wythoff1{
 	}
 	
 	/**
-	* Procedure contenant tout les methodes de test 
+	* Procedure contenant toutes les methodes de test 
 	*/
 	void methodeDeTest(){
 		testCreerPlateau();
@@ -28,7 +36,7 @@ class Wythoff1{
 	
 	/**
 	* Procedure permettant de tester la fonction creerPlateau();
-	*/ 
+	*/  
 	void testCreerPlateau() {
 		System.out.println("\n***testCreerPlateau()");
 		char[][] plateau = new char[5][5];
@@ -50,8 +58,8 @@ class Wythoff1{
 	* @param longueur : Longueur du plateau saisie par utilisateur
 	*/
 	void testCasCreerPlateau(char[][] plateau, int longueur){
-		int compteurCase = 0;
-		int nbCaseTheorique;
+		int compteurCase = 0; //Nombre de case du tableau après la fonction
+		int nbCaseTheorique; //Nombre de case du tableau calculé à la main
 		
 		System.out.print("Plateau generer de taille : " + longueur + " , ");
 		
@@ -120,29 +128,26 @@ class Wythoff1{
 		}
 		System.out.print("  ");
 		for(int comptIndice = 0;comptIndice<plateau.length;comptIndice++){
-			System.out.print( " " + comptIndice + "  ");
+			System.out.print(" " + comptIndice + " ");
 		}
 		System.out.println();
 	}
 
-
-
-
-
 	/**
 	* Procedure permettant de recevoir les entrées utilisateurs pour
 	* pouvoir l'envoyer vers placePion();
-	*/ 
-	void ajoutePion(){
-		int nbCase;
-		String mouvement;
+	* @param plateau : Le plateau de jeu
+	*/  
+	void ajoutePion(char[][] plateau) {
+		int nbCase; //Nombre de case que le joueur souhaite avancée
+		String mouvement; //Mouvement (gauche, diagonal, bas)
 		boolean flag = false;
-		//Demande le mouvement que l'utilisateur veut faire
+		
 		do {
-			mouvement = SimpleInput.getString("\nA votre tour " + joueurActuelle + " ! Bas, gauche ou diagonal ? ");
+			System.out.println("\nA votre tour " + joueurActuelle + " ! bas, gauche ou diagonal ? ");
+			mouvement = SimpleInput.getString(" ");
 		} while (!mouvement.equals("gauche") && !mouvement.equals("bas") && !mouvement.equals("diagonal"));
 
-		//Verifie que le mouvement du pion est correct et redemande tant que ça soit correct
 		do {
 			if(flag){
 				System.out.println("Entrez une valeur differentes de 0 ! ");
@@ -151,14 +156,11 @@ class Wythoff1{
 			if(nbCase == 0){
 				flag = true;
 			}
-		} while (!estPasValide(plateau, nbCase, mouvement) || nbCase <= 0);
-		
+		} while (!estPasValide(plateau, nbCase, mouvement) || nbCase < 0);
+
 		placePion(mouvement, nbCase);
 	}
 	
-	/**
-	* Procedure de test utilisant testCasPlacePion();
-	*/ 
 	void testPlacePion(){
 		System.out.println("\n***testPlacePion()");
 		plateau = creerPlateau(20);
@@ -179,15 +181,6 @@ class Wythoff1{
 		testCasPlacePion("gauche", 1, 10, 0);
 	}
 	
-	
-	/**
-	* Procedure de verification de la méthode placePion.
-	* @param mouvement : Le mouvement test
-	* @param nbCase : Distance de test
-	* @param positionXTheorique : Position X du pion theorique après l'execution du programme
-	* @param positionYTheorique : Position Y du pion theorique après l'execution du programme 
-	* 
-	*/ 
 	void testCasPlacePion(String mouvement, int nbCase, int positionXTheorique, int positionYTheorique) {
 
 		/*int initPosX = positionX;
@@ -210,6 +203,7 @@ class Wythoff1{
 			System.out.println("ERREUR");
 		}
 	}
+
 	
 	/**
 	* Procedure permettant de deplacer le pion sur le plateau de jeu
@@ -233,20 +227,97 @@ class Wythoff1{
 	}
 
 	/**
+	* Procedure permettant à "l'IA" de fonctionner correctement en 
+	* se deplaçant de façon intelligent selon le degrès de difficulté
+	* 
+	*/ 
+	void ajoutePionOrdinateur() {
+		if(difficulte == 2){
+			System.out.println("L'ordinateur joue....\n");
+			int xGagnant = -1;
+			int yGagnant = -1;
+
+			//Trouve les cases gagnantes
+			for (int i = 0; i < plateau.length; i++) {
+				for (int j = 0; j < plateau[i].length; j++) {
+					if (plateau[i][j] == 'o') {
+						xGagnant = i;
+						yGagnant = j;
+					}
+				}
+			}
+
+			if (xGagnant != -1 && yGagnant != -1) {
+				int deltaX = xGagnant - positionX; //Difference de cases entre le pion et une case gagnante dans l'axe X
+				int deltaY = yGagnant - positionY; //Difference de cases entre le pion et une case gagnante dans l'axe Y
+
+				
+				if (deltaX > 0 && positionY > 0) {
+					placePion("bas", 1);
+					placePion("gauche", 1);
+				} else if (deltaY > 0) {
+					placePion("gauche", deltaY-1);
+				} else if (positionX < plateau.length - 1) {
+					placePion("bas", deltaX-1);
+				} else {
+					placePion("bas", 1);
+					placePion("gauche", 1);
+				}
+			} else {
+				if (positionX < plateau.length - 1 || positionY > 0) {
+					if (positionX < plateau.length - 1) {
+						placePion("bas", 1);
+					} else {
+						placePion("gauche", 1);
+					}
+				} else {
+					placePion("bas", 1); 
+				}
+			}
+		}
+			
+		if(difficulte == 1){
+			
+			String mouvement = " ";
+			int nbCaseRandom;
+			
+			do{
+				int mouvementRandom = (int) (Math.random() * 3);
+				
+				if(mouvementRandom == 0){
+					mouvement = "gauche";
+				}
+				
+				if(mouvementRandom == 1){
+					mouvement = "bas";
+					
+				}
+				
+				if(mouvementRandom == 2){
+					mouvement = "diagonal";
+				}
+			
+				nbCaseRandom = (int) (Math.random() * (plateau.length - (plateau.length/2)-2) + 1);
+			}while(!estPasValide(plateau, nbCaseRandom, mouvement) || nbCaseRandom == 0);
+			
+			placePion(mouvement, nbCaseRandom);
+		}
+	}
+
+	/**
 	* Procedure de test de la methode estPasValide();
 	*/  
 	void testEstPasValide(){
 		System.out.println("\n*** testEstPasValide()");
-		testEstPasValideAvecMouvementBas();
-		testEstPasValideAvecMouvementGauche();
+		testCasEstPasValideAvecMouvementBas();
+		testCasEstPasValideAvecMouvementGauche();
 	}
-	
 	
 	/**
 	* Procedure de test de la methode estPasValide lorsque le mouvement
 	* est vers le bas.
-	*/
-	void testEstPasValideAvecMouvementBas() {
+	*/  
+	void testCasEstPasValideAvecMouvementBas() {
         char[][] plateau = creerPlateau(5);
         boolean result = estPasValide(plateau, 3, "bas");
         if (result) {
@@ -260,9 +331,9 @@ class Wythoff1{
 	* Procedure de test de la methode estPasValide lorsque le mouvement
 	* est vers la gauche.
 	*/ 
-    void testEstPasValideAvecMouvementGauche() {
+    void testCasEstPasValideAvecMouvementGauche() {
         char[][] plateau = creerPlateau(5);
-        boolean result = estPasValide(plateau, 3, "droite");
+        boolean result = estPasValide(plateau, 3, "gauche");
         if (!result) {
             System.out.println("Test OK : estPasValide avec mouvement droite");
         } else {
@@ -281,15 +352,10 @@ class Wythoff1{
 
 		if (mouvement.equals("bas")) {
 			estValide = ((positionX+nbCase) < plateau.length);
-		} else if (mouvement.equals("gauche")){
+		} else {
 			estValide = ((positionY-nbCase) >= 0);
-		} else{
-			estValide = (((positionY-nbCase) > 0) || ((positionX+nbCase) < plateau.length));
 		}
 
-		if(!estValide){
-			System.out.println("Saisissez une valeur plus petite !");
-		}
 		return(estValide);
 	}
 
@@ -311,28 +377,27 @@ class Wythoff1{
 	* @param resultatAttendu : Le resultat attendu à la sortie.
 	*/
 	void testCasChangeJoueur(String joueurActuelle, String joueur1, String joueur2, String resultatAttendu){
-		System.out.print("Joueur Actuelle : " + joueurActuelle + " | J1 : " + joueur1 + " | J2 " + joueur2 + " | JRetourne : ");
+		System.out.print("J1 : " + joueur1 + " | J2 " + joueur2 + " | JRetourne : ");
 		String joueurAvantMethode = joueurActuelle;
 		String joueurApresMethode = changeJoueur(joueurActuelle, joueur1, joueur2);
 		System.out.print(joueurApresMethode);
 
 		if(resultatAttendu == joueurApresMethode){
-			System.out.println(" | OK");
+			System.out.println("OK");
 		} else {
-			System.out.println(" | ERREUR");
+			System.out.println("ERREUR");
 		}
 	}
 
 	/**
 	* Change le joueur courant
-	* @param joueurActuelle : Le joueur qui vient de jouer
 	* @param joueur1 : Le joueur n°1
 	* @param joueur2 : Le joueur n°2
 	* @return Le joueur qui doit jouer.
 	*/
-	String changeJoueur(String joueurActuelle, String joueur1, String joueur2){
+	String changeJoueur(String joueurActuelle, String joueur1, String ordinateur){
 		if(joueurActuelle == joueur1){
-			joueurActuelle = joueur2;
+			joueurActuelle = ordinateur;
 		} else {
 			joueurActuelle = joueur1;
 		}
@@ -347,41 +412,72 @@ class Wythoff1{
 		int positionRandom = (int) (Math.random() * 2);
 		
 		if(positionRandom == 1){
-			do{
-				positionX = 0;
-				positionY = (int) (Math.random() * (plateau.length - 1) + 0);
-			}while(!(positionX != 0 || positionY != 0 && positionX != 0 || positionY != plateau.length-1));
+			positionX = 0;
+			positionY = (int) (Math.random() * (plateau.length - 1) + 0);
 		}
 		
 		if(positionRandom == 0){
-			do{
-				positionX = (int) (Math.random() * ((plateau.length-2)+1));
-				positionY = plateau.length-1;
-			}while(!((positionX != 0) || (positionY != plateau.length-1) && (positionX != plateau.length-1) || (positionY != plateau.length-1)));
+			positionX = (int) (Math.random() * ((plateau.length - 2)+1));
+			positionY = plateau.length-1;
 		}
 			plateau[positionX][positionY] = 'X';
 	}
 	
 	/**
-	* Methode executer en debut de partie pour preparer le plateau. 
+	* Procedure qui est executer une seule fois en debut de partie
+	* pour pouvoir creer un plateau de jeu et placer le pion au hasard
+	* en haut ou à droite du plateau grâce à la procedure positionnementDepart() 
 	*/
 	void demarrage(){
 		int longueur;
 		boolean flag = false;
 		do{
 			if(flag){
-				System.out.println("Le nombre doit etre superieur a 5 ! ");
+				System.out.println("\nLe nombre doit etre superieur a 5 ! ");
 			}
-			longueur = SimpleInput.getInt("Entrez la taille du tableau (doit etre superieur a 5) : ");
+			longueur = SimpleInput.getInt("\nEntrez la taille du tableau (doit etre superieur a 5) : ");
 			flag = true;
 		}while(longueur < 5);
 		plateau = creerPlateau(longueur);
-		
+		affichePositionGagnante(plateau);
 		positionnementDepart();
 		
 		affichePlateau(plateau);
 	}
 	
+	/**
+	* Procedure affichant les positions gagnantes du plateau grace
+	* à la methode positionGagnante()
+	* @param plateau : Le plateau de jeu.
+	*/
+	void affichePositionGagnante(char[][] plateau){
+        int[][] result = positionGagnante(plateau);
+        int i = 0;
+        while (i < plateau.length){
+            int x = result[i][0];
+            int y = result[i][1];
+            if (x < plateau.length && y < plateau.length ){
+                plateau[plateau.length-1-x][y] = 'o';
+                plateau[plateau.length-1-y][x] = 'o';
+            }
+            i++;
+        }
+    }
+
+	/**
+     * Les positions gagnantes sont stockées dans un tableau
+     * @param plateau : Le plateau de jeu
+     * @return resultat : Tableau double entré avec [rang][coordonées]
+     */
+    int[][] positionGagnante(char[][] plateau) {
+        int[][] resultat = new int[plateau.length][2];
+        for (int rank = 0; rank < resultat.length; rank++) {
+            resultat[rank][0] = (int) Math.floor(rank * PHI); // x = [rank*φ]
+            resultat[rank][1] = resultat[rank][0] + rank; // y = x + rank
+        }
+        return(resultat);
+    }
+
 	/**
 	* Procedure qui permet l'execution du jeu et verifie que le jeu 
 	* n'est pas terminer.
@@ -391,19 +487,26 @@ class Wythoff1{
 		int choix;
 		
 		//Recuperation du nom du joueur
-		String joueur1 = SimpleInput.getString("Nom du joueur 1 : ");
-		String joueur2 = SimpleInput.getString("Nom du joueur 2 : ");
+		String joueur1 = SimpleInput.getString("Nom du joueur : ");
+		String ordinateur = "IA";
 		System.out.println("----------------------");
 		
+		//Niveau de difficulté
+		System.out.println("1. Facile\n2. Difficile");
+		do{
+			difficulte = SimpleInput.getInt("Choisissez votre difficulte: ");
+		}while(difficulte != 2 && difficulte != 1);
+		System.out.println("---------------");
+		
 		//Choix du joueur qui va jouer en premier
-		System.out.println("1. " + joueur1 + "\n2. " + joueur2);
+		System.out.println("1. Joueur\n2. IA");
 		System.out.println("---------------");
 		do{
 			choix = SimpleInput.getInt("Qui joue en premier ? : ");
 		}while(choix != 1 && choix != 2);
 		
 		if(choix == 1){
-			joueurActuelle = joueur2;
+			joueurActuelle = ordinateur;
 		}
 		if(choix == 2){
 			joueurActuelle = joueur1;
@@ -414,50 +517,65 @@ class Wythoff1{
 			nbPartie = SimpleInput.getInt("\nCombien de partie voulez-vous faire ? ");
 		} while(nbPartie < 1);
 		
+		
+		//Boucle de la partie
 		while(compteurPartie < nbPartie+1){
 			System.out.println("\nPartie Numero " + compteurPartie + "/"+nbPartie);
-			System.out.println(joueur1 + " : " + compteurJoueur1);
-			System.out.println(joueur2 + " : " + compteurJoueur2);
+			System.out.println("Joueur : " + compteurJoueur);
+			System.out.println("Ordinateur : " + compteurIA);
 			boolean partieTerminer = false;
 			demarrage();
-			
+		
+		
+			//Boucle d'un seul tour
 			while(partieTerminer == false){
-			   joueurActuelle = changeJoueur(joueurActuelle, joueur1, joueur2);
-			   ajoutePion();
-			   affichePlateau(plateau);
+				positionGagnante(plateau);
+				joueurActuelle = changeJoueur(joueurActuelle, joueur1, ordinateur);
+				
+				if(joueurActuelle == joueur1){
+					ajoutePion(plateau);
+					affichePlateau(plateau);
+				}else if(joueurActuelle == ordinateur){
+					ajoutePionOrdinateur();
+					affichePlateau(plateau);
+				} else {
+					ajoutePion(plateau);
+					affichePlateau(plateau);
+				}
 				
 				if (positionY == 0 && positionX == plateau.length-1) {
 					System.out.println("\n");
 					System.out.println(joueurActuelle + " a gagne !");
 					
 					if(joueurActuelle == joueur1){
-						compteurJoueur1++;
+						compteurJoueur++;
 					}
-					if(joueurActuelle == joueur2){
-						compteurJoueur2++;
+					if(joueurActuelle == ordinateur){
+						compteurIA++;
 					}
 					
 					compteurPartie++;
 					
 					System.out.println("\033[H\033[2J");
 					partieTerminer = true;
+					
 				} 
 			}
 		}
 		
-		if(compteurJoueur1 > compteurJoueur2){
-				System.out.println(joueur1 + " " + " a gagne avec " + compteurJoueur1 + " points");
-			}else if(compteurJoueur1 < compteurJoueur2){
-				System.out.println(joueur2 + " " + " a gagne avec " + compteurJoueur2 + " points");
+		if(compteurIA > compteurJoueur){
+				System.out.println("L'ordinateur a gagne avec " + compteurIA + " points");
+			}else if(compteurIA < compteurJoueur){
+				System.out.println("Le joueur a gagne avec " + compteurJoueur + " points");
 			}
-		 if(compteurJoueur1 == compteurJoueur2){
+		 if(compteurIA == compteurJoueur){
 				System.out.println("Egalite");
-				System.out.println(joueur1 + " " + " a " + compteurJoueur1 + " points");
-				System.out.println(joueur2 + " " + " a " + compteurJoueur2 + " points");
+				System.out.println("Le joueur a gagne avec " + compteurJoueur + " points");
+				System.out.println("L'ordinateur a gagne avec " + compteurIA + " points");
 			}
 			
 			System.out.println("Merci d'avoir joue ! ");
-	
 	}
 	
 }
+
